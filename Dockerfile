2f8d5e9bd96ff33a2d16bba372f49096f9e22909
snigo <snigo.ogins@gmail.com>
2020-11-07 16:23:20 +0100

    Change logout query parameter

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN npm install pm2 -g
 COPY package*.json ./
 RUN npm install
 COPY . .
-CMD [ "npm", "start" ]
+CMD [ "pm2-runtime", "npm", "--", "start" ]
--- routes/logout.ctrl.js
+++ routes/logout.ctrl.js
@@ -4,9 +4,9 @@ const { badRequest, serverError } = require('./handlers');
 module.exports = async (req, res) => {
   try {
     const { cid } = req.body;
-    const { ctx } = req.query;
+    const { scope } = req.query;
     if (!cid) return badRequest(res);
-    const everywhere = ctx && ctx.toLowerCase() === 'everywhere';
+    const everywhere = scope && scope.toLowerCase() === 'everywhere';
     const response = await (everywhere ? destroyAllClients(cid) : destroyClient(cid));
     if (response.error) return serverError(res);
     res.status(200).json({ cid: null });
